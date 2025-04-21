7cdd140bf1d11a5dc7c2a669fc70fa7c195bd4c2
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2025-04-21 13:49:21 +0000

    refactor: remove redundant semi colons from queries

--- models/library.js
+++ models/library.js
@@ -1,20 +1,20 @@
 const query = require("../db")
 
 async function getAllCategories() {
-    const { rows } = await query("SELECT book_category, slug FROM book_categories;")
+    const { rows } = await query("SELECT book_category, slug FROM book_categories")
     return rows
 }
 
 async function getBooksOfCategory(category_slug) {
     const { rows } = await query(
-        "SELECT * from books inner join book_categories on books.category_id=book_categories.category_id where book_categories.slug=$1;",
+        "SELECT * from books inner join book_categories on books.category_id=book_categories.category_id where book_categories.slug=$1",
         [category_slug],
     )
     return rows
 }
 
 async function categoryExists(category_slug) {
-    const { rows } = await query("SELECT EXISTS (SELECT 1 FROM book_categories where slug=$1);", [category_slug])
+    const { rows } = await query("SELECT EXISTS (SELECT 1 FROM book_categories where slug=$1)", [category_slug])
     return rows[0].exists
 }
 
